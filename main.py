eee2ad07f2e000be40fa6255a5abc21a657be0cd
DuzhonkovaMarina <merelin.lin@gmail.com>
2024-10-31 00:54:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# кортеж
+immutable_var = ('вишня', 'гранат', 11, 13, True)
+print(immutable_var)
+immutable_var[0] = 'клубника'
+print(immutable_var)
+# невозможно изменить - кортеж
+# список
+mutable_list = ['дыня','арбуз',12,14,False]
+mutable_list[0] = 'кокос'
+print(mutable_list)
